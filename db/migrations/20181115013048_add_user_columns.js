a66cff14da65daa0835bd8261e53fae6308cfd40
Elbert <elbert.bae@gmail.com>
2018-11-15 01:49:40 +0000

    @Mark - created users columns (FK to rank and email)

--- db/migrations/20181115013048_add_user_columns.js
+++ db/migrations/20181115013048_add_user_columns.js
@@ -1,8 +1,18 @@
 
 exports.up = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.table('users', function(table){
+      table.text('email').notNullable();
+      table.integer('rank_id').unsigned();
+      table.foreign('rank_id').references('ranks.id');
+    })
+  ])
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.table('users', function(table){
+    table.dropColumn('email');
+    table.dropForeign('rank_id', 'ranks.id');
+    table.dropColumn('rank_id');
+  });
 };
