f556085ec9b4d6c048286700a9004eb0f13dbe3e
Dawson Ley <anarchonist@protonmail.com>
2018-11-15 05:08:49 +0000

    I'm comitting

--- db/seeds/users.js
+++ db/seeds/users.js
@@ -3,13 +3,13 @@ exports.seed = function(knex, Promise) {
     knex('users').del(),
     knex('ranks').del(),
     knex('categories').del(),
-    knex('events').del(),
+    knex('events').del()
   ])
     .then(function () {
       return Promise.all([
-        knex('users').insert({id: 1, name: 'Alice', email: 'testone@gmail.com', rank_id: 'admin'}),
-        knex('users').insert({id: 2, name: 'Bob', email: 'testtwo@gmail.com', rank_id: 'user'}),
-        knex('users').insert({id: 3, name: 'Charlie', email: 'testthree@gmail.com', rank_id: 'user'}),
+        knex('users').insert({id: 1, name: 'Alice', email: 'testone@gmail.com', rank_id: 1}),
+        knex('users').insert({id: 2, name: 'Bob', email: 'testtwo@gmail.com', rank_id: 2}),
+        knex('users').insert({id: 3, name: 'Charlie', email: 'testthree@gmail.com', rank_id: 2}),
 
         knex('ranks').insert({id: 1, type: 'admin'}),
         knex('ranks').insert({id: 2, type: 'user'}),
@@ -20,7 +20,7 @@ exports.seed = function(knex, Promise) {
 
         knex('events').insert({id: 1, start_date: new Date(), end_date: new Date(), detail: 'Have fun', name: 'Name One'}),
         knex('events').insert({id: 2, start_date: new Date(), end_date: new Date(), detail: 'Have fun', name: 'Name Two'}),
-        knex('events').insert({id: 3, start_date: new Date(), end_date: new Date(), detail: 'Have fun', name: 'Name Three'}),
+        knex('events').insert({id: 3, start_date: new Date(), end_date: new Date(), detail: 'Have fun', name: 'Name Three'})
       ]);
     });
 };
