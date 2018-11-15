2d9cf7d29327276cea7fadd16321bff3fc98f469
Mark Barlescu <barlescu92@gmail.com>
2018-11-15 22:44:23 +0000

    -mark, main_url added to events table, short_url added to  users_events table

---
+++ db/migrations/20181115221459_events_users-shorturl.js
@@ -0,0 +1,23 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.table('events_users', function(table) {
+      table.text('short_url')
+    }),
+    knex.schema.table('events', function(table) {
+      table.text('main_url')
+    })
+  ]);
+
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.table('events_users', function(table){
+      table.dropColumn('short_url');
+    }),
+    knex.schema.table('events', function(table){
+      table.dropColumn('main_url');
+    })
+  ])
+};
