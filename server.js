b3976d144576ab911b3be27d5db4f4b78cda5f87
Mark Barlescu <barlescu92@gmail.com>
2018-11-15 22:07:42 +0000

    @mark pulling

--- server.js
+++ server.js
@@ -43,6 +43,11 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get('/events', (req, res) => {
+  res.send('hellloo')
+})
+
+
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
 });
