b58f64d2ca6180517c8aef79afe34c3ed32d9d48
Elbert <elbert.bae@gmail.com>
2018-11-15 23:13:08 +0000

    @elbert - added framework of data entry from submits for event creation

--- routes/events.js
+++ routes/events.js
@@ -2,12 +2,38 @@
 
 const express = require('express');
 const eventRoutes  = express.Router();
+const randomURL = require('../public/scripts/urls.js');
 
 module.exports = (knex) => {
+  let name = req.body.name;
+  let email = req.body.email;
 
   eventRoutes.get("/", (req, res) => {
     res.send("This is for the AJAX request for the event creation page");
   });
+
+  eventRoutes.post("/", (req, res) => {
+    knex('users').insert({
+      name: name,
+      email: email,
+      rank_id: 1
+    });
+    res.send();
+  });
+
+  eventRoutes.post("/create", (req, res) => {
+    let eventUrl = randomURL();
+    knex('events').insert({
+      start_date: req.body.start_date,
+      end_date: req.body.end_date,
+      img_src: req.body.img,
+      detail: req.body.details,
+      name: req.body.eventName,
+      categories: req.body.category,
+      main_url: eventUrl
+    });
+  });
+
 
   eventRoutes.get("/:id", (req, res) => {
     res.send("This page should render a specific event's page");
