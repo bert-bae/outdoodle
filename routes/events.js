5507cb899825196a88c0c5da21f648ebface32e1
Elbert <elbert.bae@gmail.com>
2018-11-17 02:26:22 +0000

    finalized events_users database entry, re-render to events/:id page works

--- routes/events.js
+++ routes/events.js
@@ -3,10 +3,16 @@
 const express = require('express');
 const eventRoutes  = express.Router();
 const randomURL = require('../public/scripts/urls.js');
+const session     = require('cookie-session');
+
+eventRoutes.use(session({
+  name: 'session',
+  keys: ["TEST1", "TEST2"],
+}));
 
 module.exports = (knex) => {
-
   eventRoutes.post("/", (req, res) => {
+    req.session.temp = req.body.email;
     knex('users').select('email').where('email', req.body.email)
     .then((result) => {
       if(result.length) {
@@ -23,9 +29,8 @@ module.exports = (knex) => {
     });
   });
 
-// convert this to knex query, especially the very last WHERE line
   eventRoutes.get("/:id", (req, res) => {
-    knex.raw(`SELECT events.name AS event_name, users.name AS user_name, events.location AS location, events.start_date, events.end_date, events.detail, categories.type FROM events_users
+    return knex.raw(`SELECT events.name AS event_name, users.name AS user_name, events.location AS location, events.start_date, events.end_date, events.detail, categories.type FROM events_users
       JOIN users ON events_users.user_id = users.id
       JOIN events ON events_users.event_id = events.id
       JOIN categories ON events.categories_id = categories.id
@@ -36,7 +41,7 @@ module.exports = (knex) => {
   });
 
   eventRoutes.get("/:id/edit", (req, res) => {
-    knex.raw(`SELECT events.name AS event_name, users.name AS user_name, events.location AS location, events.start_date, events.end_date, events.detail, categories.type FROM events_users
+    return knex.raw(`SELECT events.name AS event_name, users.name AS user_name, events.location AS location, events.start_date, events.end_date, events.detail, categories.type FROM events_users
       JOIN users ON events_users.user_id = users.id
       JOIN events ON events_users.event_id = events.id
       JOIN categories ON events.categories_id = categories.id
@@ -45,6 +50,7 @@ module.exports = (knex) => {
       res.render('event', { eventData: result.rows[0] });
     });
   });
+
   eventRoutes.post("/:id/edit", (req, res) => {
     res.send("This page should render an edit page for a specific event's page");
   });
@@ -58,6 +64,8 @@ module.exports = (knex) => {
     });
   });
 
+
+
   eventRoutes.post("/create", (req, res) => {
     let eventUrl = randomURL();
     knex('events').insert({
@@ -69,14 +77,29 @@ module.exports = (knex) => {
       name: req.body.eventName,
       categories_id: req.body.category,
       main_url: eventUrl
-    }).then(() => {
+    })
+    .then(() => {
+      return Promise.all([
+        knex('events').select('id').where('main_url', eventUrl),
+        knex('users').select('id').where('email', req.session.temp),
+      ]);
+    })
+    .then((multiresult) => {
+      let event_id = multiresult[0][0].id;
+      let user_id = multiresult[1][0].id;
+      let userUrl = randomURL();
+      return knex('events_users').insert({
+        event_id: event_id,
+        user_id: user_id,
+        short_url: userUrl,
+      });
+    })
+    .then(() => {
       res.send({eventUrl: eventUrl});
     }).catch((err) => {
       res.send({error: err});
     });
   });
-
-
 
 
   return eventRoutes;
