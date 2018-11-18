6ad8278e58329cc28ea4f7f3ef7b8782cf0a1e30
Dawson Ley <anarchonist@protonmail.com>
2018-11-18 06:32:35 +0000

    still trying to fix user register

--- public/scripts/attendee.js
+++ public/scripts/attendee.js
@@ -3,9 +3,8 @@ $(document).ready(function () {
   //write an if statement so if the user has already enter there name and email they don't see this form, otherwise slidedown;
   $('.user-register-form').on('submit', function (event) {
     event.preventDefault(event);
-  });
 
-  $.ajax({
+    $.ajax({
         type: 'POST',
         url: '/events/:id/userinfo',
         data: $('.user-register-form').serialize(),
@@ -13,4 +12,5 @@ $(document).ready(function () {
 
         }
       });
+  });
 });
